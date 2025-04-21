d26862e008d76acc19575081d3c4b6069cbfe351
Quentin Heskett <heskec@rpi.edu>
2025-04-21 13:18:56 -0400

    Add files via upload

--- scripts/battle_manager.gd
+++ scripts/battle_manager.gd
@@ -2,11 +2,15 @@ extends Node
 
 const SMALL_CARD_SCALE = 0.8
 const CARD_MOVE_SPEED = 0.2
+const STARTING_HEALTH = 20
+const BATTLE_POS_OFFSET = 25
 
 var battle_timer
 var empty_basic_card_slots = []
 var opponent_cards_on_battlefield = []
 var player_cards_on_battlefield = []
+var player_health
+var opponent_health
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,6 +23,11 @@ func _ready() -> void:
 	empty_basic_card_slots.append($"../CardSlots/OpponentCardSlot5")
 	empty_basic_card_slots.append($"../CardSlots/OpponentCardSlot6")
 	empty_basic_card_slots.append($"../CardSlots/OpponentCardSlot7")
+	
+	player_health = STARTING_HEALTH
+	$"../PlayerHealth".text = str(player_health)
+	opponent_health = STARTING_HEALTH
+	$"../OpponentHealth".text = str(opponent_health)
 
 func _on_end_turn_button_pressed() -> void:
 	opponent_turn()
@@ -46,9 +55,10 @@ func opponent_turn():
 		for card in enemy_cards_to_attack:
 			if player_cards_on_battlefield.size() == 0:
 				# direct attack commander
-				direct_attack(card, "Opponent")
+				await direct_attack(card, "Opponent")
 			else:
-				attack()
+				var card_to_attack = player_cards_on_battlefield.pick_random()
+				await attack(card, card_to_attack, "Opponent")
 				
 	
 	# end turn
@@ -71,19 +81,51 @@ func direct_attack(attacking_card, attacker):
 	await wait(0.15)
 	
 	if attacker == "Opponent":
-		pass
 		#damage player
+		player_health = max(player_health - attacking_card.attack, 0)
+		$"../PlayerHealth".text = str(player_health)
 	else:
-		pass
 		# damage opponent
+		opponent_health = max(opponent_health - attacking_card.attack, 0)
+		$"../OpponentHealth".text = str(opponent_health)
 		
 	var tween2 = get_tree().create_tween()
 	tween2.tween_property(attacking_card, "position", attacking_card.card_in_slot.position, CARD_MOVE_SPEED)
+	attacking_card.z_index = 0
+	await wait(1.0)
+	
+	
+func attack(attacking_card, defending_card, attacker):
+	attacking_card.z_index = 5
+	var new_pos = Vector2(defending_card.position.x, defending_card.position.y + BATTLE_POS_OFFSET)
+	var tween = get_tree().create_tween()
+	tween.tween_property(attacking_card, "position", new_pos, CARD_MOVE_SPEED)
+	await wait(0.15)
+	var tween2 = get_tree().create_tween()
+	tween2.tween_property(attacking_card, "position", attacking_card.card_in_slot.position, CARD_MOVE_SPEED)
+	
+	# deal damage to cards
+	defending_card.health = max(0, defending_card.health - attacking_card.attack)
+	defending_card.get_node("Health").text = str(defending_card.health)
+	attacking_card.health = max(0, attacking_card.health - defending_card.attack)
+	attacking_card.get_node("Health").text = str(attacking_card.health)
+	
 	await wait(1.0)
 	attacking_card.z_index = 0
 	
-func attack():
-	print("attack")
+	# destroy dead cards
+	if attacking_card.health == 0:
+		destroy_card(attacking_card, attacker)
+	if defending_card.health == 0:
+		if attacker == "Player":
+			destroy_card(attacking_card, "Opponent")
+		else:
+			destroy_card(attacking_card, "Player")
+		
+func destroy_card(card, card_owner):
+	# remove card from field
+	# update all containers
+	pass
 
 func try_play_card():
 	# play a card
@@ -91,7 +133,7 @@ func try_play_card():
 	if opponent_hand.size() == 0:
 		end_opponent_turn()
 		return
-	var random_empty_basic_card_slot = empty_basic_card_slots[randi_range(0, empty_basic_card_slots.size()-1)]
+	var random_empty_basic_card_slot = empty_basic_card_slots.pick_random()
 	empty_basic_card_slots.erase(random_empty_basic_card_slot)
 	# temporary, play card with highest attack
 	var card_with_highest_attack = opponent_hand[0]
